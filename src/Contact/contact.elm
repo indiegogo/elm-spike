ff53967716987c0cce76293010b6458a04b2b666
Curtis Schofield <curtis.schofield@indiegogo.com>
2017-11-09 11:58:01 -0800

    basic style

--- src/Contact/contact.elm
+++ src/Contact/contact.elm
@@ -1,3 +1,34 @@
-import Contact.JSON.Contact as Contact
+module Contact exposing (fred, wilma)
+import Html
+import Contact.Json exposing (Contact)
+import Element
+import Style
+import Style.Color as Color
+import Style.Font as Font
+import Color exposing (black, white, lightGrey)
+
+fred : Contact
+fred =
+    (Contact
+        "Fred Flinstone"
+        "fred@flinstone.com"
+        "555"
+        "Cobblestone Way"
+        "Bedrock"
+    )
 
 
+wilma : Contact
+wilma =
+    (Contact
+        "Wilma Flinstone"
+        "wilma@flinstone.com"
+        "555"
+        "Cobblestone Way"
+        "Bedrock"
+    )
+
+
+
+
+        
--- src/main.elm
+++ src/main.elm
@@ -1,33 +1,123 @@
-import Html exposing (Html, div, text)
+module Main exposing (..)
+
+import Html exposing (Html, div, text, ul)
 import Contact.Json exposing (Contact)
+import Contact exposing(fred, wilma)
+import Element
+import Element.Attributes
+import Style
+import Style.Color as Color
+import Style.Font as Font
+import Style.Border as Border
+import Color exposing (black, white, lightGrey)
 
 type alias Model =
-    {
-        contacts: List Contact
+    { contacts : List Contact
     }
 
 
-
-init: (Model, Cmd msg)
+init : ( Model, Cmd msg )
 init =
-    ({ contacts = [] }, Cmd.none)
-main: Program Never Model msg
-main =
-  Html.program
-      {
-        init = init
-      , view = view
-      , update = update
-      , subscriptions = subscriptions
+    ( { contacts =
+            [ fred, wilma ]
       }
+    , Cmd.none
+    )
 
-update: msg -> Model -> (Model, Cmd msg)
+
+main : Program Never Model msg
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+update : msg -> Model -> ( Model, Cmd msg )
 update msg model =
- ( model, Cmd.none)
+    ( model, Cmd.none )
 
-view: Model -> Html msg
+-- We need a type that represents out style identifiers. 
+-- These act like css classes
+type MyStyles
+    = CardStyle | NoStyle
+
+-- We define our stylesheet
+stylesheet =
+    Style.styleSheet
+        [
+         
+         Style.style CardStyle
+            [ Color.text lightGrey
+            , Color.background black
+            , Font.size 16 -- all units given as px
+            , Font.typeface
+                [ Font.font "Helvetica"
+                , Font.font "Comic Sans"
+                , Font.font "Papyrus"
+                ]
+            ]
+          , Style.style NoStyle
+             [
+              Border.all 1
+             , Border.solid
+             ,  Color.border black
+             ]
+        ]
+
+view : Model -> Html msg
 view model =
-  div [] [ text (toString model) ]
+    Element.layout stylesheet <|
+      viewContacts CardStyle model
+
+
+viewContacts style model =
+    Element.grid
+        NoStyle
+        [Element.Attributes.spacing 1, Element.Attributes.padding 1 ]
+ { columns = [ Element.Attributes.px 100, Element.Attributes.px 100, Element.Attributes.px 100, Element.Attributes.px 100 ]
+        , rows =
+            [ Element.Attributes.px 100
+            , Element.Attributes.px 100
+            , Element.Attributes.px 100
+            , Element.Attributes.px 100
+            ]
+        , cells =
+            [ Element.cell
+                { start = ( 0, 0 )
+                , width = 1
+                , height = 1
+                , content = (Element.el NoStyle [] (Element.text "1 Name 0 0"))
+                }
+            , Element.cell
+                { start = ( 1, 1 )
+                , width = 1
+                , height = 2
+                , content = (Element.el NoStyle [ Element.Attributes.spacing 100 ] (Element.text "2 Email 1 1"))
+                }
+            , Element.cell
+                { start = ( 2, 1 )
+                , width = 2
+                , height = 2
+                , content = (Element.el NoStyle [] (Element.text "3 Address 2 1"))
+                }
+            , Element.cell
+                { start = ( 1, 0 )
+                , width = 1
+                , height = 1
+                , content = (Element.el NoStyle [] (Element.text "4 City 1 0"))
+                }
+            ]
+        }
+        --(List.map (\d -> viewContact style d ) model.contacts)
+
+
+
+viewContact style contact =
+    -- An el is the most basic element, like a <div>
+    Element.el style [] (Element.text contact.name )
 
 subscriptions : Model -> Sub msg
 subscriptions model =
