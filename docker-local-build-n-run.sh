90132843e2dc8af7f689cfcc75d23ac030507ff9
Curtis Schofield <curtis.schofield@indiegogo.com>
2017-11-10 18:16:49 -0800

    stuff

--- docker-local-build-n-run.sh
+++ docker-local-build-n-run.sh
@@ -1,11 +1,10 @@
 docker build -t elm-build -f Dockerfile.build .
 
-
-
-docker build -t elm-build -f Dockerfile.build .
 docker create -t -i --name build-container elm-build
 docker cp build-container:/app/build.tar.gz .
 docker rm -f build-container
+
 docker build -t elm-release -f Dockerfile.release .
+
 exec docker run -it -p 127.0.0.1:8000:8000 elm-release 
 
