dcd376592c1603cd9f16de25408bd79eb1716205
Curtis Schofield <curtis.schofield@indiegogo.com>
2017-11-20 17:42:26 -0800

    working towards plain old html setup

--- src/Layout.elm
+++ src/Layout.elm
@@ -14,7 +14,7 @@ bannerConfig msg width=
         ({ defaultConfig | endBgColor = Css.Colors.maroon, skewColor = Css.Colors.gray, messageBorderColor = Css.Colors.silver, mainBgColor = Css.Colors.white, width = width, message = msg })
 
 userRibbon =
-    St.div [Sa.css [Css.marginRight (Css.px -113), Css.width (Css.px 300) ] ] [
+    St.div [Sa.css [Css.marginRight (Css.px -113), Css.width (Css.px 500) ] ] [
      Ribbon.ribbon_left ( bannerConfig "Welcome User Banner" 300) 
     ]
 
@@ -23,14 +23,14 @@ tastyCodeRibbon =
         (bannerConfig "Tasty Code Cakes Banner" 300)
                     {- tasty code ribbon stuff
                       Options.css "margin-top" "25px", Options.css "margin-left" "-106px" -}
-
+styledRibbonList =
+    List.map (\(a) -> a |> St.toUnstyled) [userRibbon, tastyCodeRibbon] 
 
 view2 body links =
-    let styledRibbons =
-        St.div [] [userRibbon, tastyCodeRibbon] |> St.toUnstyled
-    in
     div [ ]
-        [ nav []  (List.concat [[styledRibbons], links])
+        [
+         header [] styledRibbonList
+         , nav []  links
          , div [] body
         ]
 
