5f90e78ba65a9b50829ca44ec194dca20907822d
Jen-Mei Wu <jenmei@indiegogo.com>
2017-12-05 13:55:36 -0800

    Update html and js files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,23 @@ import './main.css';
 import { Main } from './Main.elm';
 import registerServiceWorker from './registerServiceWorker';
 
-Main.embed(document.getElementById('root'));
+var elm = Main.embed(document.getElementById('root'));
+
+console.log(elm.ports);
+
+let elmPortAuth = elm.ports.toFirebaseAuth;
+let elmPortDB = elm.ports.toFirebaseDB;
+
+let dbElmSendPort  = elm.ports.fromFirebaseDB;
+let authElmSendPort = elm.ports.fromFirebaseAuth;
+
+if (elmPortAuth && authElmSendPort) {
+  FirebaseAuthPort(authElmSendPort, elmPortAuth);
+};
+
+if (elmPortDB && dbElmSendPort) {
+ FirebaseDBPort(dbElmSendPort, elmPortDB);
+};
+
 
 registerServiceWorker();
