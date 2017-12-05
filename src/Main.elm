6f722b7350927e258056051740f7b074eec66be5
Jen-Mei Wu <jenmei@indiegogo.com>
2017-12-05 15:00:12 -0800

    Works?

--- src/Main.elm
+++ src/Main.elm
@@ -1,55 +1,15 @@
-module Main exposing (..)
+module Main exposing (main)
 
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Core exposing (update, view, init, subscriptions, delta2url, location2messages)
+import RouteUrl as Routing
 
-
----- MODEL ----
-
-
-type alias Model =
-    {}
-
-
-init : ( Model, Cmd Msg )
-init =
-    ( {}, Cmd.none )
-
-
-
----- UPDATE ----
-
-
-type Msg
-    = NoOp
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    ( model, Cmd.none )
-
-
-
----- VIEW ----
-
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
-        ]
-
-
-
----- PROGRAM ----
-
-
-main : Program Never Model Msg
 main =
-    Html.program
-        { view = view
+    Routing.program
+        {
+         delta2url = delta2url
+        , location2messages = location2messages
         , init = init
+        , view = view
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         }
